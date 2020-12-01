bd14f81926584daa69267bd41a08c5b4ef608e8e
Yanis Guaye <yguaye44@gmail.com>
2020-12-02 00:27:43 +0100

    Initial commit

---
+++ src/day01.rs
@@ -0,0 +1,9 @@
+#[aoc_generator(day01)]
+pub fn day01_gen(_input: &str) -> Vec<i32> {
+    unimplemented!()    
+}
+
+#[aoc(day01, part1)]
+pub fn day01_part1(_input: &[i32]) -> i32 {
+    unimplemented!()    
+}
