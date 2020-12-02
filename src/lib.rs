9f83d402d38f2e261b87cb8d6bad26a9efb9a81d
Yanis Guaye <yguaye44@gmail.com>
2020-12-02 19:16:48 +0100

    Day02

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
 mod day01;
+mod day02;
 
 aoc_lib! { year = 2020 }
